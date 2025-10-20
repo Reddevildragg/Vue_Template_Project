4e5fd9a5f6d77ba5660a500af4ba0f9dca59598c
Tom <thomas.greener@hotmail.co.uk>
2025-10-20 13:14:46 +0100

    updates to fix error generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,28 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite' // v4 Vite plugin
-import checker from 'vite-plugin-checker'
-import path from 'node:path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite'; // v4 Vite plugin
+import checker from 'vite-plugin-checker';
+import path from 'path';
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        tailwindcss(),
-        checker({
-            typescript: true,
-            vueTsc: true,
-            eslint: {
-                // tell the plugin we’re on ESLint 9 flat config
-                useFlatConfig: true,
-                lintCommand: 'eslint "./src/**/*.{ts,tsx,vue}"' // optional but I like to be explicit
-            },
-            stylelint: {lintCommand: 'stylelint "./**/*.{css,vue}"'},
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    checker({
+      typescript: true,
+      vueTsc: true,
+      eslint: {
+        // tell the plugin we’re on ESLint 9 flat config
+        useFlatConfig: true,
+        lintCommand: 'eslint "./src/!**!/!*.{ts,tsx,vue}"', // optional but I like to be explicit
+      },
+      stylelint: { lintCommand: 'stylelint "./**/*.{css,vue}"' },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-})
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
+  },
+});
