efc4d7546483dc0a895511d616f9866f4f293ea3
Tom <thomas.greener@hotmail.co.uk>
2025-12-23 16:43:03 +0000

    removed the old setup files that are not needed.

--- scripts/templates/component-library/src/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from 'vue';
-import {{PASCAL_PLUGIN_NAME}}Component from './{{PASCAL_PLUGIN_NAME}}Component.vue';
-
-export default {
-  install(app: App) {
-    app.component('{{PASCAL_PLUGIN_NAME}}Component', {{PASCAL_PLUGIN_NAME}}Component);
-    console.log('{{PLUGIN_NAME}} installed');
-  }
-};
